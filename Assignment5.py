299a5267001044f3b5ba1c44989732813a06ddcb
swapnakotyal-alt <swapnakotyal@gmail.com>
2026-01-07 21:42:29 +0530

    Add files via upload

---
+++ Assignment5.py
@@ -0,0 +1,12 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+mark1 = int(input("Enter marks of subject 1: "))
+mark2 = int(input("Enter marks of subject 2: "))
+mark3 = int(input("Enter marks of subject 3: "))
+
+average = (mark1 + mark2 + mark3) / 3
+
+print("Name:", name)
+print("Age:", age)
+print("Average Marks:", average)
---
+++ assignment1.py
@@ -0,0 +1,9 @@
+name = "John"
+age = 20
+height = 1.75
+is_student = True
+
+print("Name:", name, "| Type:", type(name))
+print("Age:", age, "| Type:", type(age))
+print("Height:", height, "| Type:", type(height))
+print("Is Student:", is_student, "| Type:", type(is_student))
