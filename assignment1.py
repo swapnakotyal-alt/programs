a6e4c22db4bf4c5045a7d62ced4cfd7d095abe67
swapnakotyal-alt <swapnakotyal@gmail.com>
2026-01-08 10:24:14 +0530

    Update personal information in assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,9 +1,9 @@
-name = "John"
+name = "swapna"
 age = 20
-height = 1.75
+height = 4.75
 is_student = True
-
 print("Name:", name, "| Type:", type(name))
 print("Age:", age, "| Type:", type(age))
 print("Height:", height, "| Type:", type(height))
 print("Is Student:", is_student, "| Type:", type(is_student))
+
